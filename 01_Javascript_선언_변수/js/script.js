a0483387ece797476c26ce6241327c72a3d8c5be
28sungmin <gooddays4001@naver.com>
2024-02-17 22:33:47 +0900

    until alert

---
+++ 01_Javascript_선언_변수/js/script.js
@@ -0,0 +1,8 @@
+document.write("▶ 주석 설명<br />");
+document.write("// : 한 줄 주석 처리", "<br />");
+document.write("/* */ : 여러 줄 주석 처리");
+document.write("<br /><br />");
+
+document.write("▶ 자바스크립트를 통한 브라우저에서 제공되는 팝업창 형태<br />");
+document.write("#1. 경고창 : 경고 메시지를 제공 → 디자인이 다 달라서 잘 쓰지는 않는다.<br />");
+alert("이곳은 경고창입니다.");
